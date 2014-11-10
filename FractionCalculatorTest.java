3ee543abd0835547fb676ed29765829161fb879f
abellj01 <lognuk@netscape.net>
2014-11-10 23:43:22 +0000

    Update FractionCalculatorTest.java

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -1,19 +1,35 @@
 package fractionAy;
 
+/**
+ * This class takes an input from the user and splits into fractions
+ * that get computed using operations in the class FractionCalculator
+ * @author AbJr
+ * @version 101114
+ */
+
 import java.util.Scanner;
 
 public class FractionCalculatorTest {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+	Scanner input = new Scanner(System.in);
 		System.out.println("0");
 		System.out.println("Hi Aymar");
-		System.out.println("Please enter a positive integer ");
-		int num = input.nextInt();
-		System.out.println("Please enter another a positive integer ");
-		int denom = input.nextInt();
-		Fraction f = new Fraction(num, denom);
-		System.out.println(f);
+		System.out.print("Please enter an expression: ");
+		String expression = input.nextLine();
+		int n = expression.length();
+		StringTokenizer expressionbits = new StringTokenizer(expression, " ");
+		String[] frac = new String [n];
+		
+		for (int i = 0; i<frac.length; i++){
+			frac[i] = expressionbits.nextToken();
+			StringTokenizer fracbits = new StringTokenizer(frac[i], "/");
+			String fracnum = fracbits.nextToken();
+			String fracdenom = fracbits.nextToken();
+			System.out.println(frac[i]);
+			System.out.println(fracnum);
+			System.out.println(fracdenom);
+		}
 		
 		input.close();
 	}
